406c8d4b5d1fcf5ad3be4071641000e89e95b1c9
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-14 18:12:45 -0600

    Up

--- models/res_users.py
+++ models/res_users.py
@@ -5,6 +5,9 @@ class ResUsers(models.Model):
 
     additional_visible_users = fields.Many2many(
         'res.users',
+        'res_users_additional_visibility_rel',  # Nombre de la tabla intermedia
+        'user_id',  # Columna que apunta al registro actual (origen)
+        'visible_user_id',  # Columna que apunta al usuario adicional (destino)
         string="Usuarios adicionales para visibilidad",
         help="Define qué usuarios adicionales pueden ver los documentos creados por este usuario."
     )
